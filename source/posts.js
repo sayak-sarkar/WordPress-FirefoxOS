8bccaf5d545776c6917ce003661a1455651c9bf2
sayak-sarkar <sayak.bugsmith@gmail.com>
2013-09-22 17:52:55 +0530

    Live Posts List and Post Refresh Implemented.

--- source/posts.js
+++ source/posts.js
@@ -53,8 +53,10 @@ enyo.kind({
 				},
 			]},
 			{name: "postContainer", style: "position: relative;", fit: true, components: [
+				{id: "outerContainer", tag: "div"}
 				//Posts List Definition
-				{
+				
+				/*{
 					name: "postList",
 					kind: "List",
 					fit: true,
@@ -83,11 +85,11 @@ enyo.kind({
 							]
 						}
 					]
-				}
+				}*/
 			]},
 		]}
 	],
-	postDatasource: [
+	/*postDatasource: [
 		{name: "Installing Sublime Text 2 on Fedora 19 – Schrodinger’s Cat", gist: "Here’s another three step guide to installing Sublime Text 2 on Fedora 19 – Schrodinger’s Cat:- Download the installation script from the following gist. https://gist.github.com/sayak-sarkar/5810101 Extract it to your home directory [or anywhere you like]. $tar -xvf gist5810101-3b0e9bb3ef5128760df9e3e06877fa4f7e5689ec.tar.gz Open your terminal (preferably as super user), navigate to your home directory and execute the shell script."},
 		{name: "Installing VLC player on Fedora 19 – Schrodinger’s Cat", gist: "Here’s a simple three step guide to installing VLC media player on Fedora 19 [Schrodinger's Cat]:- Login as Super User: $su Setup rpmfusion: #rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm Install vlc using the default yum package manager: #yum install vlc mozilla-vlc Voila! You now have VLC media player installed on you computer! Filed under: Fedora, How-to Guides, Technology,"},
 		{name: "Porting WordPress for WebOS to Firefox OS", gist: "Introduction to WordPress for WebOS WordPress for WebOS was a web app which used the WebOS platform’s innovative “Cards” feature, which allowed users to quickly switch between managing, editing, and browsing content. WordPress for WebOS was built around Sliding Panels that enables users to easily switch between creating, editing and managing content. The app allowed "},
@@ -98,7 +100,7 @@ enyo.kind({
 		{name: "Debugging NO KEY warning during “yum install vlc”", gist: "This is a problem that I encountered today while tryiing to install VLC media player on my brand new Fedora 18 installation. Right after the dependency resolutions are complete, yum returns the following warning:- Transaction Summary =============================================== Install 1 Package (+45 Dependent packages) Total size: 37 M Installed size: 118 M Is this ok [y/N]:"},
 		{name: "LAMP Setup for Fedora 18", gist: "Easy steps to setup Fedora 18 as a LAMP server containing Apache, PHP, Perl, Python, Ruby and MySQL. P.S.: This requires root access to the shell. Step 1: Open terminal as root user and enter the following: #yum install -y httpd php php-mysql php-gd php-imap php-ldap php-odbc php-pear php-xml php-xmlrpc php-magickwand php-mbstring php-mcrypt php-mssql php-shout"},
 		{name: "Adobe Flex Builder 3 Download", gist: "Reblogged from Chris Tierney: I had to track down a download location for Adobe's Flex 3 install today. It was a little hard to find so I thought I'd share this link for a Windows install: http://download.macromedia.com/pub/flex/flex_builder/FB3_win.exe Why it's at a macromedia.com address I have no clue, but hope this helps someone out. This defaults"}
-	],
+	],*/
 	menuDatasource: [
 		{name: "Reader"},
 		{name: "Posts"},
@@ -110,7 +112,7 @@ enyo.kind({
 	create: function () {
 		getPosts();
 		this.inherited(arguments);
-		this.$.postList.setCount(this.postDatasource.length);
+		/*this.$.postList.setCount(this.postDatasource.length);*/
 		this.$.menuList.setCount(this.menuDatasource.length);
 	},
 	setupMenuItem: function (inSender, inEvent) {
@@ -139,12 +141,12 @@ enyo.kind({
 			alert("Functionality on its way!");	
 		};
 	},
-	setupPostItem: function (inSender, inEvent) {
+	/*setupPostItem: function (inSender, inEvent) {
 		this.childName = this.postDatasource[inEvent.index].name;
 		this.$.postTitle.setContent(this.childName);
-	},
+	},*/
 	listItemTap:function(inSender, inEvent) {
-		alert(this.postDatasource[inEvent.index].gist);
+		/*alert(this.postDatasource[inEvent.index].gist);*/
 	},
 	newPostTap: function(inSender, inEvent) {
 		new wp.PostCompose().renderInto(document.body);
@@ -152,8 +154,8 @@ enyo.kind({
 	drawerTap: function (inSender, inEvent) {
 		this.$.menuDrawer.setOpen(!this.$.menuDrawer.open);
 	},
-	stub: function(inSender, inEvent) {
-		this.$.main.addContent("<br/>");
+	refresh: function(inSender, inEvent) {
+		new wp.Posts().renderInto(document.body);
 	}
 });
 
@@ -186,26 +188,71 @@ function makePostRequest(url, data) {
 }
 
 function handlePostSuccess(xhr) {
-	//document.getElementById("results").innerHTML = xhr.responseText;
-
 	var parser = new XMLRPCParser(xhr.response);
 	var json = parser.toObject();
 	var fault = parser.fault;
 	console.log(fault);
-
+	var idData = [];
+	var statusData = [];
+	var titleData = [];
 	if (json instanceof Array) {
 		for (var i = 0; i < json.length; i++) {
 			var obj = json[i];
 			for(var key in obj) {
+				if (key == "post_id") {
+					idData.push(obj[key]);
+				}
+				if (key == "post_title") {
+					titleData.push(obj[key]);
+				}
+				if (key == "post_status") {
+					if (obj[key] == "publish") {
+						statusData.push("Published");
+					}
+					else if (obj[key] == "draft") {
+						statusData.push("Draft");
+					}
+					else if (obj[key] == "private") {
+						statusData.push("Private");
+					}
+				}
 				console.log(key, obj[key]);
 			}
+		}
+		var listContainer = document.createElement("div");
+		document.getElementById("outerContainer").appendChild(listContainer);
+		var listElement = document.createElement("div");
+		listContainer.appendChild(listElement);
+		listElement.className = "listItemContainer";
+
+		for (var i = 0; i < json.length; i++) {
+			
+			var itemId = document.createElement("div");
+			itemId.innerHTML = idData[i];
+			listElement.appendChild(itemId);
+			itemId.className = "itemId";
+
+			var itemTitle = document.createElement("div");
+			itemTitle.innerHTML = titleData[i];
+			listElement.appendChild(itemTitle);
+			itemTitle.className = "itemTitle";
+			
+			var itemStatus = document.createElement("div");
+			itemStatus.innerHTML = statusData[i];
+			listElement.appendChild(itemStatus);
+			itemStatus.className = "itemStatus";
+
+			listElement.appendChild(document.createElement("hr"));
+
+			console.log(idData[i]);
+			console.log(statusData[i]);
+			console.log(titleData[i]);
 		}
 	} else {
 		 console.log(json);
 	}
-	
 }
 
 function handlePostError(xhr) {
-//	document.getElementById("results").innerHTML = "Error: " + xhr.statusText;
+	alert("Error: " + xhr.statusText);
 }
